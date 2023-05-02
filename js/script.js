26e73a2fcf438a9fd43eaf51ef6509a081e3282a
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-05-02 17:18:38 +0000

    edited js and html

--- js/script.js
+++ js/script.js
@@ -9,8 +9,12 @@
 function myButtonClicked () {
   // this function shows the book price depending on genre and number of bookmarks selected
   // input
-  var tax = 0.13
-  var cost = 0
+  const TAX = 0.13
+  let cost = 0
+  let subTotal = 0
+  let taxPaid = 0
+  let total = 0
+  let price = 0
   const genre = document.getElementById("book-genre").value
   const numberOfBookmarks = parseInt(document.getElementById("number-of-bookmarks").value)
 
@@ -18,26 +22,29 @@ function myButtonClicked () {
   if (genre == "Mystery") {
     cost = 15.99
   } else if (genre == "Romance") {
-    cost = 18.50
+    cost = 19.50
   } else if (genre == "Adventure") {
     cost = 21.99
-  } else if (genre == "Fictional") {
+  } else if (genre == "Fiction") {
     cost = 24.50
   }
 
   if (numberOfBookmarks == 1) {
-    cost = 1.99
+    price = 1.99
   } else if (numberOfBookmarks == 2) {
-    cost = 2.99
+    price = 2.99
   } else if (numberOfBookmarks == 3) {
-    cost = 3.99
+    price = 3.99
   } else if (numberOfBookmarks == 4) {
-    cost = 4.99
+    price = 4.99
   }
 
+  subTotal = price + cost
+  taxPaid = subTotal * TAX
+  total = subTotal + taxPaid
+
   // output
-  document.getElementById("cost").innerHTML = "The cost is: $" + cost.toFixed(2)
-  document.getElementById("tax").innerHTML = "The tax is: $" + (cost * tax).toFixed(2)
-  document.getElementById("total").innerHTML = "The total is: $" + (cost + (cost * tax)).toFixed(2)
+  document.getElementById("cost").innerHTML = "The subtotal is: $" + subTotal.toFixed(2) + 
+  "<br />The tax is: $" + taxPaid.toFixed(2) + "<br />The total is: $" + total.toFixed(2)
 
 }
